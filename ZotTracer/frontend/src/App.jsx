24014f9b2135ac358c108c2ff0928df9c957824d
Audrey Lam <audreylam989@gmail.com>
2024-11-21 01:40:50 -0800

    add logs for excalidraw events
    Co-authored-by: Noakai Aronesty <naronesty@users.noreply.github.com>
    Co-authored-by: andy <andy-tonthat@users.noreply.github.com>

--- ZotTracer/frontend/src/App.jsx
+++ ZotTracer/frontend/src/App.jsx
@@ -8,7 +8,19 @@ import ExcalidrawComponent from './components/Excalidraw'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [shape, setShape] = useState('circle');
+  const [shape, setShape] = useState('circle'); // default shape: circle
+  const [file, setFile] = useState();// useState("./assets/StarOutline.png");
+
+  function uploadFile(e) {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
+
+  // var uploadedSvg = (
+  //   <Icon>
+  //     <img src={file}/>
+  //   </Icon>
+  // )
 
   return (
       <div id='display'>
@@ -19,11 +31,13 @@ function App() {
             <div id='shapes'>
               <Button class='shapeButton' onClick={() => setShape("circle")}/>
               <Button class='shapeButton' variant="outlined" onClick={() => setShape("triangle")}/>
-              <Button class='shapeButton' variant="contained" onClick={() => setShape("square")}/>
+              {/* <Button class='shapeButton' startIcon={<Icon><img src={file}/></Icon>}></Button> */}
+              <label for="file-upload" class="upload-label">Upload your own svg image!</label>
+              <input type="file" accept=".svg" id='file-upload' onChange={uploadFile}/>
             </div>
           </div>
           <div id='drawing_area'>
-            <p>Draw a {shape}</p>
+            <p>Draw a {shape} :D</p>
             <div id="canvas" >
             <ExcalidrawComponent />
             </div>
@@ -31,6 +45,7 @@ function App() {
         </div>
         <div id='footer'>
           <p>Score:</p>
+          <img src={file} />
         </div>
       </div>
   )
--- ZotTracer/frontend/src/components/Excalidraw.jsx
+++ ZotTracer/frontend/src/components/Excalidraw.jsx
@@ -3,14 +3,41 @@ import { Excalidraw } from "@excalidraw/excalidraw";
 
 const ExcalidrawComponent = () => {
   const excalidrawRef = useRef(null);
+  const excalidrawAPI = excalidrawRef.current;
+
+//   useEffect(() => {
+//     // Once the component is mounted, set the tool and hide the toolbar
+//     
+//     if (excalidrawAPI) {
+//       // Start with free-draw tool
+//       
+//       // Hide the toolbar
+//       excalidrawAPI.setState({
+//         appState: {
+//           isToolBarVisible: false,
+//         },
+//       });
+//     }
+//   }, []); // Only run once after mount
 
   return (
-    <div style={{ width: "100%", height: "400px", border: "1px solid #ccc", zIndex:-100}}>
+    <div style={{ width: "100%", height: "400px", border: "1px solid #ccc"}}>
       <Excalidraw 
-        ref={excalidrawRef} 
+        ref={excalidrawRef}
+        onChange={(elements) => {
+            // Handle changes to the elements
+            console.log(elements);
+        }}
+        //   onPointerUpdate={(pointerData) => {
+        //     // Handle pointer (mouse) updates
+        //     console.log(pointerData);
+        // }}
+        
         initialData={{ 
-            appState: {viewBackgroundColor: 'transparent'
-
+            appState: {
+                viewBackgroundColor: 'transparent',
+                zenModeEnabled: true,
+                isToolBarVisible: false
         } }}
       />
     </div>
