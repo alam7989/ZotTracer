4b9452aee7968af87da871cfb9f71d5fc617b5af
Audrey Lam <audreylam989@gmail.com>
2024-11-22 05:28:13 -0800

    more image processing

--- ZotTracer/backend/process_image.py
+++ ZotTracer/backend/process_image.py
@@ -14,20 +14,24 @@ def getMask(file):
     input_point = np.array([[len(image)/2, len(image)/2]]) # select middle of the image
     input_label = np.array([1])
 
-    sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")
-    predictor = SamPredictor(sam)
+    plt.figure(figsize=(10,10))
+    plt.imshow(image)
+    plt.axis('on')
+    plt.show()  
 
-    mask, score, logits = predictor.predict(
-        point_coords=input_point,
-        point_labels=input_label,
-        multimask_output=True, # generate 3 masks
-    )
+    # sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")
+    # predictor = SamPredictor(sam)
 
-    # save the mask as a png to a temporary file
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        plt.imsave(temp_file.name, mask[-1]) # use the 3rd mask
+    # mask, score, logits = predictor.predict(
+    #     point_coords=input_point,
+    #     point_labels=input_label,
+    #     multimask_output=True, # generate 3 masks
+    # )
 
-        # get the outline of the mask
-        greyscale_img = cv2.imread(temp_file.name, cv2.IMREAD_GRAYSCALE)
-        edges = cv2.Canny(greyscale_img, threshold1=50, threshold2=150) # edge detection
+    # # save the mask as a png to a temporary file
+    # with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+    #     plt.imsave(temp_file.name, mask[-1]) # use the 3rd mask
 
+    #     # get the outline of the mask
+    #     greyscale_img = cv2.imread(temp_file.name, cv2.IMREAD_GRAYSCALE)
+    #     edges = cv2.Canny(greyscale_img, threshold1=50, threshold2=150) # edge detection
