a2b4298b9e6bc7606779c7a08b08e4c838b24fa8
Noakai Aronesty <noakai@gmail.com>
2024-11-22 04:14:31 -0800

    merge fix

--- ZotTracer/frontend/src/App.jsx
+++ ZotTracer/frontend/src/App.jsx
@@ -39,64 +39,64 @@ function App() {
   };
 
   return (
-      <div id='display'>
-        <h1>ZotTracer</h1>
-        <div id='top'>
-          <div id='left'>
-            <h3>Choose your shape!</h3>
-            <div id='shapes'>
-              <Button class='shapeButton' onClick={() => setShape("a circle")}/>
-              <Button class='shapeButton' onClick={() => setShape("a triangle")}/>
-         
-              <input
-                id="file-upload"
-                type="file"
-                accept=".svg"
-                ref={fileInputRef}
-                onChange={handleFileUpload}
-                style={{ marginBottom: "10px" }}
-              />
-              <Button
-                class='shapeButton'
-                sx={{
-                  backgroundImage: `url(${uploadedImage})`,
-                  backgroundSize: "contain",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                  height: "15vh",
-                  marginTop: "1vh"
-                }}
-                onClick={() => fileInputRef.current.click()}
-              >
-                {!uploadedImage && "Upload Image (svg only)"}
-              </Button>
-            </div>
-          </div>
-        </div>
+    <div id='display'>
+    <h1 class='pageheader'>ZotTracer</h1>
+    <div id='top'>
+      <div id='left'>
+        <h3>Choose your shape!</h3>
+        <div id='shapes'>
+          <Button class='shapeButton' onClick={() => setShape("a circle")} />
+          <Button class='shapeButton' onClick={() => setShape("a triangle")} />
 
-        <div id='drawing_area'>
-          <p>Draw {shape} :D</p>
-          <div id="canvas" >
-            <ExcalidrawComponent onAPIReady={(api) => {
-              excalidrawAPIRef.current = api; // Store API in a ref
-            }} />
-          </div>
+          <input
+            id="file-upload"
+            type="file"
+            accept=".svg"
+            ref={fileInputRef}
+            onChange={handleFileUpload}
+            style={{ marginBottom: "10px" }}
+          />
+          <Button
+            class='shapeButton'
+            sx={{
+              backgroundImage: `url(${uploadedImage})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              height: "15vh",
+              marginTop: "1vh"
+            }}
+            onClick={() => fileInputRef.current.click()}
+          >
+            {!uploadedImage && "Upload Image (svg only)"}
+          </Button>
+
         </div>
       </div>
-      <div id='top2ah'>
-        <div id='footer'>
-          <Button id='canvasClearButton' class='clearButton' style={{display: "inline"}} onClick={() => {
-            document.getElementById("footer").style.display = "none";
-            if (excalidrawAPIRef.current) {
-              excalidrawAPIRef.current.updateScene({ elements: [] });
-            }
-            document.getElementById("excalidraw").style.pointerEvents = "auto";
-          }} >Clear drawing</Button>
-          <p style={{display: "inline", paddingLeft: "1.5vw"}}>Score:</p>
+
+      <div id='drawing_area'>
+        <p>Draw {shape} :D</p>
+        <div id="canvas" >
+          <ExcalidrawComponent onAPIReady={(api) => {
+            excalidrawAPIRef.current = api; // Store API in a ref
+          }} />
         </div>
-        <div id='spacer'>vhgh</div>
       </div>
     </div>
+    <div id='top2ah'>
+      <div id='footer'>
+        <Button id='canvasClearButton' class='clearButton' style={{display: "inline"}} onClick={() => {
+          document.getElementById("footer").style.display = "none";
+          if (excalidrawAPIRef.current) {
+            excalidrawAPIRef.current.updateScene({ elements: [] });
+          }
+          document.getElementById("excalidraw").style.pointerEvents = "auto";
+        }} >Clear drawing</Button>
+        <p style={{display: "inline", paddingLeft: "1.5vw"}}>Score:</p>
+      </div>
+      <div id='spacer'>vhgh</div>
+    </div>
+  </div>
   )
 }
 
